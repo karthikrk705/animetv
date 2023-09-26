f791c02171364f7a22477a887e1dcf4cdbaa53f9
karthikrk705 <karthikrk705@gmail.com>
2023-09-26 19:37:22 +0530

    001

--- src/components/frontpage.js
+++ src/components/frontpage.js
@@ -1,20 +1,11 @@
-import styles from './fpstyle.module.css';
-import image from './atvlogo.png'
+
 import { Link } from 'react-router-dom';
 
 function Firstpage(){
     return (
-        <body className={styles.background}>
-            <div className={styles.holder}>
-            <div className={styles.mpholder}>
-            <img className={styles.logo1} src={image} alt='nopic'/>
-            <Link to='/main' className='linkstyle'>
-            <button className={styles.button} >OPEN THE WORLD OF ANIME</button>
-            </Link>
-            
-            </div>
-            </div>
-        </body>
+        <Link to='/main' className='linkstyle'>
+            <button >OPEN THE WORLD OF ANIME</button>
+        </Link>
 )
 }
 
--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -1,4 +1,4 @@
-import styles from './fpstyle.module.css';
+
 import { Link } from 'react-router-dom';
 
 function Mainpage()
@@ -6,7 +6,7 @@ function Mainpage()
     return(<div>
         <h1>THIS IS THE MAIN PAGE</h1>
         <Link to='/'>
-            <button className={styles.button} >OPEN THE WORLD OF ANIME</button>
+            <button >OPEN THE WORLD OF ANIME</button>
         </Link>
         </div>
     )
